7242db57a6f85bbd551aca9a0e1aad6562e1ce9a
Otierra <alejandroverastegui@icloud.com>
2023-06-06 22:12:07 -0500

    Programa Fusionado

---
+++ ProgramaFusion.cpp
@@ -0,0 +1,12 @@
+ #include <iostream>
+
+    using namespace std;
+
+    int main() {
+
+        cout<<"Omar Alejandro Verastegui Cortes 23110310"<<endl;
+        cout<<"Mauro Rene Casillas Robles 22310300"<<endl;
+        cout<<"Santiago Roman Martinez 230110299"<<endl;
+        cout<<"Said Ezequiel Camacho Cedano 23110330"<<endl;
+        cout<<"Garcia Fernandez Victor Daniel 23110333"<<endl;
+    }
